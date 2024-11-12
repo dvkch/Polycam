8a1ba9ccba9b9e74edf88cfd6ae38dd67acd6c0d
Stanislas Chevallier <contact@stanislaschevallier.fr>
2024-11-12 22:59:44 +0100

    Update PTZ write command output

--- Sources/PTZ/Command/WriteCommand.swift
+++ Sources/PTZ/Command/WriteCommand.swift
@@ -5,8 +5,8 @@
 //  Created by syan on 06/11/2024.
 //
 
-import Foundation
 import ArgumentParser
+import Foundation
 import PTZCamera
 import PTZMessaging
 
@@ -16,28 +16,30 @@ struct WriteCommand: ParsableCommand {
         commandName: "write",
         discussion: "Available operations:\n" + availableOperations
     )
-    
+
     static var availableOperations: String {
         var operations = ["boot", "pause=secs", "position=preset(...)", "preset(...)=position"]
         operations += PTZConfig.knownWriteableStates.map({ $0.cliWriteDescription })
         operations += PTZConfig.knownWriteableComboStates.map({ $0.cliWriteDescription })
         return operations.sorted().map({ "  " + $0 }).joined(separator: "\n")
     }
-    
+
     @Option(name: .customLong("device"), help: "PTZ serial device name")
     var serial: String?
-    
+
     @Option(name: .customLong("log"), help: "Log level")
     var logLevel: LogLevel = .error
-    
+
     @Option(name: .customLong("continue"), help: "Continue on failure")
     var continueOnFailure: Bool = true
 
-    typealias Operation = (String, (Camera) throws -> ())
-    
-    @Argument(help: "Operations (boot, pause=secs, or any state(variant)=value)", transform: { try Self.parse(operation: $0) })
+    typealias Operation = (String, (Camera) throws -> Void)
+
+    @Argument(
+        help: "Operations (boot, pause=secs, or any state(variant)=value)",
+        transform: { try Self.parse(operation: $0) })
     var operations: [Operation] = []
-    
+
     private static func parse(operation operationString: String) throws -> Operation {
         let regex = #/(?<state>[a-zA-Z0-9]+)(?:\((?<variant>[a-zA-Z0-9/]+)\))?(?:=(?<value>.*))?/#
 
@@ -48,26 +50,35 @@ struct WriteCommand: ParsableCommand {
         if operation.0 == "boot" {
             return ("boot", { try $0.powerOn() })
         }
-        
+
         if operation.state == "preset", operation.value == "position" {
             guard let preset = PTZPreset(from: operation.variant.map(String.init) ?? "") else {
                 throw ValidationError("Invalid parameters for preset operation")
             }
-            return (String(operation.0), {
-                let position = try $0.position()
-                try $0.setPreset(position, for: preset)
-            })
+            return (
+                String(operation.0),
+                {
+                    let position = try $0.position()
+                    try $0.setPreset(position, for: preset)
+                }
+            )
         }
-        
-        if operation.state == "position", let value = operation.value.map(String.init), value.hasPrefix("preset(") {
-            let presetString = value.replacingOccurrences(of: "preset(", with: "").replacingOccurrences(of: ")", with: "")
+
+        if operation.state == "position", let value = operation.value.map(String.init),
+            value.hasPrefix("preset(")
+        {
+            let presetString = value.replacingOccurrences(of: "preset(", with: "")
+                .replacingOccurrences(of: ")", with: "")
             guard let preset = PTZPreset(from: presetString) else {
                 throw ValidationError("Invalid parameters for position operation")
             }
-            return (String(operation.0), {
-                let presetPosition = try $0.preset(for: preset)
-                try $0.setPosition(presetPosition)
-            })
+            return (
+                String(operation.0),
+                {
+                    let presetPosition = try $0.preset(for: preset)
+                    try $0.setPosition(presetPosition)
+                }
+            )
         }
 
         if operation.state == "pause" {
@@ -76,16 +87,25 @@ struct WriteCommand: ParsableCommand {
             }
             return ("pause=\(seconds)", { _ in Thread.sleep(forTimeInterval: seconds) })
         }
-        
-        if let stateType = PTZConfig.knownWriteableComboStates.first(where: { $0.name.camelCased == operation.state }) {
-            guard let state = stateType.init(valueString: operation.value.map(String.init) ?? "") else {
+
+        if let stateType = PTZConfig.knownWriteableComboStates.first(where: {
+            $0.name.camelCased == operation.state
+        }) {
+            guard let state = stateType.init(valueString: operation.value.map(String.init) ?? "")
+            else {
                 throw ValidationError("Invalid parameters for state \"\(operation.state)\"")
             }
             return (state.description, { try $0.set(state) })
         }
 
-        if let stateType = PTZConfig.knownWriteableStates.first(where: { $0.name.camelCased == operation.state }) {
-            guard let state = stateType.init(valueString: operation.value.map(String.init) ?? "", variantString: operation.variant.map(String.init) ?? "") else {
+        if let stateType = PTZConfig.knownWriteableStates.first(where: {
+            $0.name.camelCased == operation.state
+        }) {
+            guard
+                let state = stateType.init(
+                    valueString: operation.value.map(String.init) ?? "",
+                    variantString: operation.variant.map(String.init) ?? "")
+            else {
                 throw ValidationError("Invalid parameters for state \"\(operation.state)\"")
             }
             return (state.description, { try $0.set(state) })
@@ -103,16 +123,13 @@ struct WriteCommand: ParsableCommand {
 
         for (name, action) in operations {
             do {
-                print("-> \(name)", terminator: "")
-
                 let d = Date()
                 try action(camera)
                 let t = Int(Date().timeIntervalSince(d) * 1000)
-                print(", \(t)ms")
-            }
-            catch {
-                print(", failed \(error)")
-                
+                print("-> \(name), \(t)ms")
+            } catch {
+                print("-> \(name), failed \(error)")
+
                 if !continueOnFailure {
                     throw ExitCode.failure
                 }
