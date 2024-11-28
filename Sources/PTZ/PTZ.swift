27f1116ca5416fd01304219b07b4c71e74669420
Stanislas Chevallier <contact@stanislaschevallier.fr>
2024-11-28 22:50:49 +0100

    Version bump 1.2.0

--- Sources/PTZ/PTZ.swift
+++ Sources/PTZ/PTZ.swift
@@ -22,7 +22,7 @@ struct PTZ: ParsableCommand {
         return CommandConfiguration(
             commandName: "ptz",
             abstract: "PTZ",
-            version: "1.1.0",
+            version: "1.2.0",
             subcommands: commands
         )
     }
