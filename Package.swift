09bd43868123afb3c74dceec5742e7752beb185c
Syan <contact@syan.me>
2025-08-24 19:11:09 +0200

    Update Package

--- Package.swift
+++ Package.swift
@@ -1,13 +1,13 @@
 // swift-tools-version: 5.9
 
-import PackageDescription
 import CompilerPluginSupport
+import PackageDescription
 
 let package = Package(
     name: "PTZ",
     platforms: [.macOS(.v13)],
     products: [
-        .executable(name: "ptz", targets: ["PTZ"]),
+        .executable(name: "ptz", targets: ["PTZ"])
     ],
     dependencies: [
         .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
@@ -16,25 +16,35 @@ let package = Package(
         .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "510.0.3"),
     ],
     targets: [
-        .executableTarget(name: "PTZ", dependencies: [
+        .executableTarget(
+            name: "PTZ",
+            dependencies: [
                 .target(name: "PTZCamera"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
-                .product(name: "SwiftCurses", package: "SwiftCurses")
-        ]),
-        .target(name: "PTZCamera", dependencies: [
-            .target(name: "PTZMessaging"),
-            .target(name: "PTZCameraMacros"),
-        ]),
-        .target(name: "PTZMessaging", dependencies: [
-            .product(name: "SwiftSerial", package: "swiftserial"),
-        ]),
-        .macro(name: "PTZCameraMacros", dependencies: [
-            .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
-            .product(name: "SwiftCompilerPlugin", package: "swift-syntax")
-        ]),
-        .testTarget(name: "PTZCameraTests", dependencies: ["PTZCamera"], resources: [
-            .copy("Fixtures/Data")
-        ]),
-        .testTarget(name: "PTZTests", dependencies: ["PTZ"])
+                .product(name: "SwiftCurses", package: "SwiftCurses"),
+            ]),
+        .target(
+            name: "PTZCamera",
+            dependencies: [
+                .target(name: "PTZMessaging"),
+                .target(name: "PTZCameraMacros"),
+            ]),
+        .target(
+            name: "PTZMessaging",
+            dependencies: [
+                .product(name: "SwiftSerial", package: "swiftserial")
+            ]),
+        .macro(
+            name: "PTZCameraMacros",
+            dependencies: [
+                .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
+                .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
+            ]),
+        .testTarget(
+            name: "PTZCameraTests", dependencies: ["PTZCamera"],
+            resources: [
+                .copy("Fixtures/Data")
+            ]),
+        .testTarget(name: "PTZTests", dependencies: ["PTZ"]),
     ]
 )
