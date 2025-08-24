c3dc97ddad652eaec7330f53a4b8e2e1e6942240
Syan <contact@syan.me>
2025-08-24 20:11:09 +0200

    Build static std lib for linux, bump to 1.3.0

--- Package.swift
+++ Package.swift
@@ -10,10 +10,10 @@ let package = Package(
         .executable(name: "ptz", targets: ["PTZ"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.2.2"),
-        .package(url: "https://github.com/mredig/SwiftSerial.git", from: "0.1.4"),
+        .package(url: "https://github.com/apple/swift-argument-parser.git", from: "1.3.0"),
+        .package(url: "https://github.com/mredig/SwiftSerial.git", from: "0.1.5"),
         .package(url: "https://github.com/Jomy10/SwiftCurses.git", branch: "master"),
-        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "510.0.3"),
+        .package(url: "https://github.com/swiftlang/swift-syntax.git", from: "600.0.1"),
     ],
     targets: [
         .executableTarget(
@@ -22,29 +22,34 @@ let package = Package(
                 .target(name: "PTZCamera"),
                 .product(name: "ArgumentParser", package: "swift-argument-parser"),
                 .product(name: "SwiftCurses", package: "SwiftCurses"),
-            ]),
+            ]
+        ),
         .target(
             name: "PTZCamera",
             dependencies: [
                 .target(name: "PTZMessaging"),
                 .target(name: "PTZCameraMacros"),
-            ]),
+            ]
+        ),
         .target(
             name: "PTZMessaging",
             dependencies: [
                 .product(name: "SwiftSerial", package: "swiftserial")
-            ]),
+            ]
+        ),
         .macro(
             name: "PTZCameraMacros",
             dependencies: [
                 .product(name: "SwiftSyntaxMacros", package: "swift-syntax"),
                 .product(name: "SwiftCompilerPlugin", package: "swift-syntax"),
-            ]),
+            ]
+        ),
         .testTarget(
             name: "PTZCameraTests", dependencies: ["PTZCamera"],
             resources: [
                 .copy("Fixtures/Data")
-            ]),
+            ]
+        ),
         .testTarget(name: "PTZTests", dependencies: ["PTZ"]),
     ]
 )
